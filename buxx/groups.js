b3611262cd7126e8e14ccf55ec0ffde9122bedce
卜星星 <boxingxing@limikeji.com>
2021-03-04 17:06:21 +0800

    增加查找队长功能

--- buxx/groups.js
+++ buxx/groups.js
@@ -2,4 +2,15 @@ const groups = [
     ['败家仔の奎顺', '败家仔の延扶', '败家仔の甫见', '败家仔の念朋', '败家仔の初养'],
 ]
 
+export function getLeader(name) {
+    let leader = ''
+    for (const i in groups) {
+        if (groups[i].indexOf(name) == -1) {
+            continue
+        }
+        leader = groups[i][4]
+    }
+    return leader
+}
+
 export default groups
